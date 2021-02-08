3bcc4a50db64feb3befe9b4546979022770c72d3
Julieta Fernandez <juli47g1@stud.kea.dk>
2021-02-08 16:20:38 +0100

    dynamic web 1st iteration

---
+++ product.js
@@ -0,0 +1,33 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1163";
+
+// fetch the data
+fetch(url)
+  .then((res) => res.json())
+
+  .then((data) => showProduct(data));
+
+// populate the page
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".index .brand").textContent = product.brandname;
+  document.querySelector(".index .category").textContent = product.category;
+  document.querySelector(".index .sub-category").textContent =
+    product.subcategory;
+  document.querySelector(".index .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(".index .gender").textContent = product.gender;
+
+  document.querySelector(".productInfo .price").textContent = product.price;
+  document.querySelector(".productInfo .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(".productInfo .info").textContent =
+    product.description;
+  document.querySelector(".productInfo .sub-category").textContent =
+    product.subcategory;
+
+  document.querySelector(
+    ".productimg"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector(".productimg").alt = product.productdisplayname;
+}
