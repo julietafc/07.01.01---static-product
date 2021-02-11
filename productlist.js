62f083e269b0834a0240d2cda99b94418fd06adf
Julieta Fernandez <juli47g1@stud.kea.dk>
2021-02-11 20:18:41 +0100

    productlist from scratch

--- productlist.js
+++ productlist.js
@@ -1,53 +1,51 @@
-// const url = "https://kea-alt-del.dk/t7/api/products";
+const url = "https://kea-alt-del.dk/t7/api/products";
 
 // // fetch the data
-// fetch(url)
-//   // simplify > .then((res) => res.json())
-//   .then(function (res) {
-//     return res.json();
-//   })
-//   // simplify > .then((data) => showProduct(data));
-//   .then(function (data) {
-//     handleProductList(data);
-//   });
+fetch(url)
+  //   // simplify > .then((res) => res.json())
+  .then(function (res) {
+    return res.json();
+  })
+  //   // simplify > .then((data) => showProduct(data));
+  .then(function (data) {
+    handleProductList(data);
+  });
 
-// function handleProductList(data) {
-//   //   console.log(data);
-//   data.forEach(showProduct);
-// }
+function handleProductList(data) {
+  //   console.log(data);
+  data.forEach(showProduct);
+}
 
-// function showProduct(product) {
-//   console.log(product);
-//   // grab the template
-//   const template = document.querySelector("#productTemplate").content;
-//   // clone it
-//   const copy = template.cloneNode(true);
-//   // change content
-//   copy.querySelector("h3").textContent = product.productdisplayname;
-//   copy.querySelector(
-//     "img"
-//   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-//   copy.querySelector(
-//     "img"
-//   ).alt = `${product.brandname} ${product.productdisplayname}`;
-//   copy.querySelector(".price span").textContent = product.price;
-//   copy.querySelector(
-//     ".subtle"
-//   ).textContent = `${product.articletype} | ${product.brandname}`;
-
-//   if (product.soldout) {
-//     copy.querySelector("article").classList.add("soldOut");
-//   }
-
-//   if (product.discount) {
-//     copy.querySelector("article").classList.add("onSale");
-//     copy.querySelector(".discounted p").textContent =
-//       "DKK " + (product.price - product.price / product.discount).toFixed();
-//     copy.querySelector(".discounted p:nth-child(2)").textContent =
-//       product.discount + "%";
-//   }
-//   // grab parent
-//   const parent = document.querySelector("main");
-//   // append
-//   parent.appendChild(copy);
-// }
+function showProduct(product) {
+  console.log(product);
+  //   // grab the template
+  const template = document.querySelector("#productTemplate").content;
+  //   // clone it
+  const copy = template.cloneNode(true);
+  //   // change content
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector(
+    "img"
+  ).alt = `${product.brandname} ${product.productdisplayname}`;
+  copy.querySelector(".price span").textContent = product.price;
+  copy.querySelector(
+    ".subtle"
+  ).textContent = `${product.articletype} | ${product.brandname}`;
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (product.discount) {
+    copy.querySelector("article").classList.add("onSale");
+    copy.querySelector(".discounted p").textContent =
+      "DKK " + (product.price - product.price / product.discount).toFixed();
+    copy.querySelector(".discounted p:nth-child(2)").textContent =
+      product.discount + "%";
+  }
+  //   // grab parent
+  const parent = document.querySelector("main");
+  //   // append
+  parent.appendChild(copy);
+}
