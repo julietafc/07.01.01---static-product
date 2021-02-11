1d0855bcb7230cf9071f069f04da7ea5219daa96
Julieta Fernandez <juli47g1@stud.kea.dk>
2021-02-11 19:58:10 +0100

    going back and starting  a bit from scratch

--- product.js
+++ product.js
@@ -1,35 +1,38 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-const url = "https://kea-alt-del.dk/t7/api/products/" + id;
+const url = `https://kea-alt-del.dk/t7/api/products/2801`;
 
-// fetch the data
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get("id");
+// const url = "https://kea-alt-del.dk/t7/api/products/" + id;
+
+// // fetch the data
 fetch(url)
   .then((res) => res.json())
-
   .then((data) => showProduct(data));
 
-// populate the page
+// // populate the page
 
 function showProduct(product) {
   console.log(product);
-  document.querySelector(".index .brand").textContent = product.brandname;
-  document.querySelector(".index .category").textContent = product.category;
+  document.querySelector(".index .brand").textContent =
+    product.brandname + " |";
   document.querySelector(".index .sub-category").textContent =
-    product.subcategory;
+    product.subcategory + " |";
+  document.querySelector(".index .gender").textContent = product.gender + " |";
   document.querySelector(".index .productname").textContent =
     product.productdisplayname;
-  document.querySelector(".index .gender").textContent = product.gender;
-
-  document.querySelector(".productInfo .price").textContent = product.price;
-  document.querySelector(".productInfo .productname").textContent =
-    product.productdisplayname;
-  document.querySelector(".productInfo .info").textContent =
-    product.description;
-  document.querySelector(".productInfo .sub-category").textContent =
-    product.subcategory;
-
   document.querySelector(
     "img.productimg"
   ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
   document.querySelector(".productimg").alt = product.productdisplayname;
+
+  document.querySelector(".productInfo .brand").textContent = product.brandname;
+  document.querySelector(".productInfo .sub-category").textContent =
+    product.subcategory;
+  document.querySelector(".productInfo .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(".productInfo .prev-price").textContent =
+    "DKK " + product.price;
+
+  //   document.querySelector(".productInfo .info").textContent =
+  //     product.description;
 }
