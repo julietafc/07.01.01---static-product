8e2feddb3d37bf6bfb90f166f44fa1f8c6ab1a89
Julieta Fernandez <juli47g1@stud.kea.dk>
2021-02-11 21:38:18 +0100

    nav letter working and subcategories in propper order

--- subcategories.js
+++ subcategories.js
@@ -1,27 +1,60 @@
-// // //fetch data
+// create navigation
+// <li><a href="#letter_a">A</a></li>
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const letterArray = letters.split("");
+console.log(letterArray);
+letterArray.forEach(handleLetter);
+
+function handleLetter(letter) {
+  //create nav link
+  createNavLink(letter);
+
+  //create section
+  createSubCatSection(letter);
+}
+
+function createSubCatSection(letter) {
+  const template = document.querySelector("#sectionTemplate").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector("h2").textContent = letter;
+  clone.querySelector("section").id = `letter_${letter}`;
+  document.querySelector(".subcatList").appendChild(clone);
+}
+
+function createNavLink(letter) {
+  const template = document.querySelector("#linkTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("a").textContent = letter;
+  copy.querySelector("a").href = `letter_${letter}`;
+  document.querySelector(".index ol").appendChild(copy);
+}
+
+//fetch data
 fetch("http://kea-alt-del.dk/t7/api/subcategories")
   .then((res) => res.json())
   .then(gotData);
 
-// // //loop through
+//loop through
 function gotData(data) {
   data.forEach(showSubCategory);
 }
 
 function showSubCategory(item) {
   //   console.log(item.subcategory);
-  // //   //grab
-  const template = document.querySelector("template").content;
-  // //   //clone
+  //grab
+  const template = document.querySelector("#linkTemplate").content;
+  //clone
   const copy = template.cloneNode(true);
-  // //   //change
+  //change
   copy.querySelector("a").textContent = item.subcategory;
   copy.querySelector(
     "a"
   ).href = `productlist.html?subcategory=${item.subcategory}`;
-  // //   //parent
-  const topParent = document.querySelector("#letter_a");
+  //parent
+  const firstLetter = item.subcategory[0].toLowerCase();
+  const topParent = document.querySelector(`#letter_${firstLetter}`);
   const elemParent = topParent.querySelector("ol");
-  // //   //append
+  //append
   elemParent.appendChild(copy);
 }
